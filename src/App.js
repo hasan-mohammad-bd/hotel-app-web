09cafb86a29de59b31b6bf9f1aa85d2c6f8d4002
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-04-12 12:44:02 +0600

    adding header

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/packages" element={<Packages></Packages>}></Route>
         <Route path="/checkout" element={<CheckOut></CheckOut>}></Route>
--- src/conponents/Header/Header.js
+++ src/conponents/Header/Header.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import "./Header.css"
+import React from "react";
+import "./Header.css";
+import logo from "../../images/logo (2).png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            <h2>this is header</h2>
+  return (
+    <div className="navbar">
+      <div className="d-flex container justify-content-between align-items-center py-2">
+        <div className="img-holder">
+          <img className="img-fluid" src={logo} alt="" />
         </div>
-    );
+        <nav>
+          <Link to="/home">Home</Link>
+          <Link to="/packages">Packages</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
